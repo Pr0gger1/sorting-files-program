62a7fc011da55e677c788e6912a55130bc45c560
Pr0gger1 <kraus2202200491@gmail.com>
2022-07-22 13:56:37 +0300

    all comments was retyped, system files was removed

--- FileSorter.py
+++ FileSorter.py
@@ -14,58 +14,65 @@ extensions_category = {
     'Документы': ['pdf', 'txt', 'rtf', 'epub', 'doc', 'docx', 'docm'],
     'Архивы': ['zip', 'rar', '7z', 'tar', 'arj', 'iso'],
     'Аудио': ['mp3', 'ogg', 'wav', 'flac', 'aac'],
-    'Скрипты': ['py', 'js', 'jar'],
+    'Скрипты': ['py', 'js', 'jar', 'php'],
     'Иконки': ['ico'],
     'Шрифты': ['otf', 'ttf', 'fon', 'fnt'],
     'Исполняемые файлы': ['exe', 'msi', 'bat'],
-    'Системные файлы': ['sys', 'reg'],
-    'Презентации': ['pptx, ppt'],
+    'Презентации': ['pptx', 'ppt'],
     'Excel-таблицы': ['xlsx', 'xls'],
     'Торренты': ['torrent'],
     'Веб-страницы': ['html', 'htm']
 }
 
 class FileSorter():
-
   def __init__(self, file_categoryDict: dict, current_rootPath: str):
     self.root = tk.Tk()
     self.root.withdraw()
     
-    self.file_categoryDict = file_categoryDict
-    self.current_rootPath = current_rootPath
+    self.file_categoryDict: dict = file_categoryDict
+    self.current_rootPath: str = current_rootPath
+    
+    
+  def __get_extension(self, file: str) -> str:
+    return file.split(".")[0]
   
-  def get_subfolder_paths(self) -> list:                # <-- получение папок из текущего каталога
+# getting folders from current directory
+  def __get_subfolder_paths(self) -> list:
     return [f.path for f in os.scandir(self.current_rootPath) if f.is_dir()]
 
-  def create_folders_with_categories(self):             # <-- создание папок под категории файлов
+# creating folders for category files
+  def create_folders_with_categories(self) -> None:
     for folder in self.file_categoryDict:
       if not os.path.exists(f'{self.current_rootPath}\\{folder}'):
         os.mkdir(folder)
 
-  def get_file_paths(self) -> list:                     # <-- получение полного пути файлов, лежащих в каталоге
+
+# getting the absolute path to files in a directory
+  def __get_file_paths(self) -> list:
     return [file for file in os.listdir(self.current_rootPath) if os.path.isfile(file)]
 
-  def sort_files(self):       # <-- алгоритм сортировки файлов
-    file_paths = self.get_file_paths()
+# sorting files algorihtm
+  def sort_files(self) -> None:
+    files: list = self.__get_file_paths()
 
     try:
-      for file in file_paths:
-        file_path = os.path.join(self.current_rootPath, file)
-        file_extension = file.split('.')[-1]
+      for file in files:
+        file_path: str = os.path.join(self.current_rootPath, file)
+        file_extension: str = self.__get_extension(file)
 
-        #пропуск программы-сортировщика
-        if file_path.lower() == sys.argv[0]:
+        # skip executable program
+        if file_path.lower() == sys.argv[0].lower():
           continue
 
         for type_data, ext in self.file_categoryDict.items():
-          if (file_extension in ext):
-            shutil.move(file, f'{self.current_rootPath}\\{type_data}\\{file}')
+          if file_extension in ext:
+            shutil.move(file, f"{self.current_rootPath}\\{type_data}\\{file}")
 
-    except:
+    except Exception:
       messagebox.showerror("Ошибка!", f"Не удалось сортировать некоторые файлы\n{traceback.format_exc()}")
           
   def del_empty_dir(self):
-    subfolder_path = self.get_subfolder_paths()
+    subfolder_path: list = self.__get_subfolder_paths()
 
     for dir in subfolder_path:
       if not os.listdir(dir):
@@ -73,10 +80,13 @@ class FileSorter():
         os.rmdir(dir)
 
 if __name__ == '__main__':
-  #Получение каталога, с которого был запущен скрипт
-  current_dir = os.getcwd()
+  # getting path to current directory
+  current_dir: str = os.getcwd()
 
-  sorter = FileSorter(file_categoryDict = extensions_category, current_rootPath = current_dir)
+  sorter = FileSorter(
+    file_categoryDict = extensions_category, 
+    current_rootPath = current_dir
+    )
   sorter.create_folders_with_categories()
   sorter.sort_files()
   sorter.del_empty_dir()
